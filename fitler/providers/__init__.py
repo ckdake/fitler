2686c14eede4ab316f9a87d60f9aece2fef8f55f
Chris Kelly <ckdake@ckdake.com>
2025-07-29 01:10:38 +0000

    Cleanup

--- fitler/providers/__init__.py
+++ fitler/providers/__init__.py
@@ -1,11 +1,11 @@
 """Fitness service provider integrations for fitler."""
 
-from .spreadsheet import SpreadsheetProvider
-from .strava import StravaProvider
-from .ridewithgps import RideWithGPSProvider
-from .stravajson import StravaJsonProvider
-from .garmin import GarminProvider
-from .file import FileProvider
+from .spreadsheet.spreadsheet_provider import SpreadsheetProvider
+from .strava.strava_provider import StravaProvider
+from .ridewithgps.ridewithgps_provider import RideWithGPSProvider
+from .stravajson.stravajson_provider import StravaJsonProvider
+from .garmin.garmin_provider import GarminProvider
+from .file.file_provider import FileProvider
 
 __all__ = [
     "SpreadsheetProvider",
